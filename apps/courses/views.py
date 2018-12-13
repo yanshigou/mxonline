fb11496072046f7711bcfec16381cb4a65ba6aea
CMXOffice <569578851@qq.com>
2018-12-13 17:52:28 +0800

    课程详情页 单独收藏功能 和相关推荐

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from .models import Course
+from operation.models import UserFavorite
 # Create your views here.
 
 
@@ -45,4 +46,23 @@ class CourseDetailView(View):
         # 正价课程点击数
         course.click_nums += 1
         course.save()
-        return render(request, "course-detail.html", locals())
+
+        has_fav_course = False
+        has_fav_org = False
+
+        if request.user.is_authenticated():
+            if UserFavorite.objects.filter(user=request.user, fav_id=course_id, fav_type=1):
+                has_fav_course = True
+            if UserFavorite.objects.filter(user=request.user, fav_id=course.course_org_id, fav_type=2):
+                has_fav_org = True
+        tag = course.tag
+        if tag:
+            relate_courses = Course.objects.filter(tag=tag)[:2]
+        else:
+            relate_courses = []
+        return render(request, "course-detail.html", {
+            "course": course,
+            "relate_courses": relate_courses,
+            "has_fav_course": has_fav_course,
+            "has_fav_org": has_fav_org
+        })
