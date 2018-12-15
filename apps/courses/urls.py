512271e3ec367e417a1f9b35e08ee079b143bb9c
yanshigou_hasee <569578851@qq.com>
2018-12-15 23:01:21 +0800

    增加课程评论

--- apps/courses/urls.py
+++ apps/courses/urls.py
@@ -14,4 +14,9 @@ urlpatterns = [
 
     url(r'^info/(?P<course_id>\d+)/$', CourseInfoView.as_view(), name='course_info'),
 
+    # 课程评论
+    url(r'^comments/(?P<course_id>\d+)/$', CommentsView.as_view(), name='course_comments'),
+
+    # 添加课程评论
+    url(r'^add_comments/$', AddCommentsView.as_view(), name='course_add_comments'),
 ]
--- apps/courses/views.py
+++ apps/courses/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 from django.views.generic.base import View
 from pure_pagination import Paginator, EmptyPage, PageNotAnInteger
 from .models import Course, CourseResource
-from operation.models import UserFavorite
+from operation.models import UserFavorite, CourseComments, UserCourse
+from django.http import HttpResponse, JsonResponse
+from utils.mixin_utils import LoginRequiredMixin
 # Create your views here.
 
 
@@ -68,14 +70,78 @@ class CourseDetailView(View):
         })
 
 
-class CourseInfoView(View):
+# LoginRequiredMixin 对登录进行验证
+class CourseInfoView(LoginRequiredMixin, View):
     """
     课程章节信息
     """
     def get(self, request, course_id):
         course = Course.objects.get(id=int(course_id))
+
+        # 查询用户是否已经关联了该课程
+        usercourses = UserCourse.objects.filter(user=request.user, course=course)
+        if not usercourses:
+            usercourse = UserCourse.objects.create(user=request.user, course=course)
+            usercourse.save()
+
+        user_courses = UserCourse.objects.filter(course=course)
+        user_ids = [user_course.user.id for user_course in user_courses]
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        # 取出所有课程id
+        course_ids = [user_course.course.id for user_course in all_user_courses]
+        # 该用户学过的其他课程
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by('-click_nums')[:5]
         all_resources = CourseResource.objects.filter(course=course)
         return render(request, "course-video.html", {
             "course": course,
-            "course_resources": all_resources
+            "course_resources": all_resources,
+            "relate_courses": relate_courses
         })
+
+
+class CommentsView(LoginRequiredMixin, View):
+    def get(self, request, course_id):
+        course = Course.objects.get(id=int(course_id))
+        all_resources = CourseResource.objects.filter(course=course)
+        all_comments = CourseComments.objects.filter(course_id=course_id).order_by("-id")
+
+        # 查询用户是否已经关联了该课程
+        usercourses = UserCourse.objects.filter(user=request.user, course=course)
+        if not usercourses:
+            usercourse = UserCourse.objects.create(user=request.user, course=course)
+            usercourse.save()
+
+        user_courses = UserCourse.objects.filter(course=course)
+        user_ids = [user_course.user.id for user_course in user_courses]
+        all_user_courses = UserCourse.objects.filter(user_id__in=user_ids)
+        # 取出所有课程id
+        course_ids = [user_course.course.id for user_course in all_user_courses]
+        # 该用户学过的其他课程
+        relate_courses = Course.objects.filter(id__in=course_ids).order_by('-click_nums')[:5]
+        return render(request, "course-comment.html", {
+            "course": course,
+            "course_resources": all_resources,
+            "all_comments": all_comments,
+            "relate_courses": relate_courses
+        })
+
+
+class AddCommentsView(View):
+    """
+    用户添加课程评论
+    """
+    def post(self, request):
+        if not request.user.is_authenticated():
+            # 判断用户登录状态
+            return JsonResponse({'status': 'fail', 'msg': '用户未登录'})
+        course_id = request.POST.get('course_id', 0)
+        comments = request.POST.get('comments', "")
+        if int(course_id) > 0 and comments:
+            course_comments = CourseComments()
+            course = Course.objects.get(id=int(course_id))
+            course_comments.course = course
+            course_comments.comments = comments
+            course_comments.user = request.user
+            course_comments.save()
+            return JsonResponse({'status': 'success', 'msg': '添加成功'})
+        return JsonResponse({'status': 'fail', 'msg': 't添加失败'})
