25c4c95bcdb06f22588bf2261d3a81030df17c0c
yanshigou <569578851@qq.com>
2018-12-20 22:23:57 +0800

    简单修改

--- apps/courses/views.py
+++ apps/courses/views.py
@@ -6,6 +6,7 @@ from .models import Course, CourseResource, Video
 from operation.models import UserFavorite, CourseComments, UserCourse
 from django.http import HttpResponse, JsonResponse
 from utils.mixin_utils import LoginRequiredMixin
+from django.db.models import Q
 # Create your views here.
 
 
@@ -14,6 +15,12 @@ class CourseListView(View):
         all_courses = Course.objects.all().order_by("-add_time")
 
         hot_courses = Course.objects.all().order_by("-students")[:5]
+
+        # 课程搜索
+        search_keywords = request.GET.get('keywords', '')
+        if search_keywords:
+            all_courses = all_courses.filter(Q(name__icontains=search_keywords) | Q(desc__icontains=search_keywords) |
+                                             Q(detail__icontains=search_keywords))
 
         # 课程排序
         sort = request.GET.get('sort', '')
--- apps/users/views.py
+++ apps/users/views.py
@@ -2,7 +2,9 @@
 from django.shortcuts import render
 from django.contrib.auth import authenticate, login
 from django.contrib.auth.backends import ModelBackend
+# or
 from django.db.models import Q
+
 from django.views.generic.base import View
 from django.contrib.auth.hashers import make_password
 
