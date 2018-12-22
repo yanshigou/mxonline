f07bd3f147ccc2e0235581533d21069bd847b148
yanshigou <569578851@qq.com>
2018-12-22 23:29:29 +0800

    修改个人信息form

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -31,3 +31,9 @@ class UploadImageForm(forms.ModelForm):
     class Meta:
         model = UserProfile
         fields = ['image']
+
+
+class UserInfoForm(forms.ModelForm):
+    class Meta:
+        model = UserProfile
+        fields = ['nick_name', 'birday', 'gender', 'mobile', 'address']
