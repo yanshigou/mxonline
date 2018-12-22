2fed0acb06744e34eb401666ad33cca4bcc81a04
yanshigou <569578851@qq.com>
2018-12-22 23:06:03 +0800

    添加type

--- apps/users/models.py
+++ apps/users/models.py
@@ -28,7 +28,8 @@ class UserProfile(AbstractUser):
 class EmailVerifyRecord(models.Model):
     code = models.CharField(max_length=20, verbose_name='验证码')
     email = models.EmailField(max_length=50, verbose_name='邮箱')
-    send_type = models.CharField(max_length=10, choices=(("register", '注册'),("forget", '找回密码')), verbose_name='验证码类型')
+    send_type = models.CharField(max_length=100, choices=(("register", '注册'), ("forget", '找回密码'),
+                                                         ("update_email", '修改邮箱')), verbose_name='验证码类型')
     send_time = models.DateTimeField(default=datetime.now, verbose_name='发送时间')
 
     class Meta:
