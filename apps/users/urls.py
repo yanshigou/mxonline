6d125c2102f22c925c455f3f71fc31382be94d0d
yanshigou <569578851@qq.com>
2018-12-25 23:11:39 +0800

    配置url

--- apps/users/urls.py
+++ apps/users/urls.py
@@ -4,7 +4,8 @@ __date__ = '2018/12/21 23:48'
 
 
 from django.conf.urls import url
-from .views import UserInfoView, UploadImageView, UpdatePwdView, SendEmailCodeView, UpdateEmailView
+from .views import UserInfoView, UploadImageView, UpdatePwdView, SendEmailCodeView, UpdateEmailView, MyCourses
+from .views import MyFavOrgView, MyFavTeacherView, MyFavCourseView
 
 urlpatterns = [
     # 用户信息
@@ -23,4 +24,15 @@ urlpatterns = [
     # 修改邮箱
     url(r'^update_email/$', UpdateEmailView.as_view(), name='update_email'),
 
+    # 我的课程
+    url(r'^mycourses/$', MyCourses.as_view(), name='mycourses'),
+
+    # 我的收藏 课程机构
+    url(r'^myfav/org/$', MyFavOrgView.as_view(), name='myfav_org'),
+
+    # 我的收藏 教师
+    url(r'^myfav/teacher/$', MyFavTeacherView.as_view(), name='myfav_teacher'),
+
+    # 我的收藏 课程
+    url(r'^myfav/course/$', MyFavCourseView.as_view(), name='myfav_course'),
 ]
