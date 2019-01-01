c97647f5c016a71b466f77563c631f2c27a78a94
yanshigou <569578851@qq.com>
2019-01-01 22:42:27 +0800

    新增字段

--- apps/courses/models.py
+++ apps/courses/models.py
@@ -21,6 +21,7 @@ class Course(models.Model):
     youneed_know = models.CharField(max_length=300, default='', verbose_name=u'课程须知')
     teacher_tell = models.CharField(max_length=300, default='', verbose_name=u'老师告诉你能学到什么')
     add_time = models.DateTimeField(default=datetime.now, verbose_name=u'添加时间')
+    is_banner = models.BooleanField(default=False, verbose_name=u'是否轮播')
 
     class Meta:
         verbose_name = u'课程'
