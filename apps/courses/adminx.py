a5fcaa41569447a79b3fa1c84fc2722f610b3280
CMXOffice <569578851@qq.com>
2019-01-08 16:42:58 +0800

    xadmin的多种功能开发，多个管理器等

--- apps/courses/adminx.py
+++ apps/courses/adminx.py
@@ -1,15 +1,57 @@
+# -*- coding: utf-8 -*-
 __author__ = 'dzt'
 __date__ = '2018/09/09 13:16'
 
 
 import xadmin
-from .models import Course, Lesson, Video, CourseResource
+from .models import Course, Lesson, Video, CourseResource, BannerCourse
+from organization.models import CourseOrg
+
+
+class LessonInline(object):
+    model = Lesson
+    extra = 0
+
+
+class CourseResourceInline(object):
+    model = CourseResource
+    extra = 0
 
 
 class CourseAdmin(object):
-    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+    # 函数也可以显示 get_zj_nums
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students', 'get_zj_nums', 'go_to']
     search_fields = ['name', 'desc', 'detail', 'degree', 'students']
     list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+
+    # 排序
+    ordering = ['-students']
+    # 只读
+    readonly_fields = ['students']
+    # 不显示
+    exclude = []
+    # 页面组装 把章节加入到课程页面 可多个 但只能做一层嵌套
+    inlines = [LessonInline, CourseResourceInline]
+    # 列表页可以编辑
+    list_editable = ['degree']
+    # 自动页面刷新时间
+    refresh_times = [3, 5, 10]
+
+    # 分管理器显示指定数据
+    def queryset(self):
+        qs = super(CourseAdmin, self).queryset()
+        qs = qs.filter(is_banner=False)
+        return qs
+
+    def save_models(self):
+        # 在保存课程的时候统计课程机构的课程数
+        obj = self.new_obj
+        obj.save()
+        if obj.course_org is not None:
+            course_org = obj.course_org
+            course_org.course_nums = Course.objects.filter(course_org=course_org).count()
+            course_org.save()
+
 
 
 class LessonAdmin(object):
@@ -30,7 +72,29 @@ class CourseResourceAdmin(object):
     list_filter = ['course', 'name', 'download', 'add_time']
 
 
+class BannerCourseAdmin(object):
+    list_display = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+    search_fields = ['name', 'desc', 'detail', 'degree', 'students']
+    list_filter = ['name', 'desc', 'detail', 'degree', 'learn_times', 'students']
+
+    # 排序
+    ordering = ['-students']
+    # 只读
+    readonly_fields = ['students']
+    # 不显示
+    exclude = []
+    # 页面组装 把章节加入到课程页面 可多个 但只能做一层嵌套
+    inlines = [LessonInline, CourseResourceInline]
+
+    # 分管理器显示指定数据
+    def queryset(self):
+        qs = super(BannerCourseAdmin, self).queryset()
+        qs = qs.filter(is_banner=True)
+        return qs
+
+
 xadmin.site.register(Course, CourseAdmin)
+xadmin.site.register(BannerCourse, BannerCourseAdmin)
 xadmin.site.register(Lesson, LessonAdmin)
 xadmin.site.register(Video, VideoAdmin)
 xadmin.site.register(CourseResource, CourseResourceAdmin)
