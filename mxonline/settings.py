03d72e1593fab4ffc3440ce33a014039bcdd0f5c
cmx_company <569578851@qq.com>
2019-03-14 14:10:35 +0800

    apps.users

--- mxonline/settings.py
+++ mxonline/settings.py
@@ -43,7 +43,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'users',
+    'apps.users',
     'courses',
     'organization',
     'operation',
