8e5f793f220c5068b5b76a6e1eb056afefeb78ed
cmx_company <569578851@qq.com>
2019-05-07 18:16:52 +0800

    修改课程轮播个数

--- apps/users/views.py
+++ apps/users/views.py
@@ -326,7 +326,7 @@ class IndexView(View):
         # 取出轮播图
         all_banners = Banner.objects.all().order_by('index')
         courses = Course.objects.filter(is_banner=False)[:6]
-        banner_courses = Course.objects.filter(is_banner=True)[:3]
+        banner_courses = Course.objects.filter(is_banner=True)[:5]
         course_orgs = CourseOrg.objects.all()[:15]
         return render(request, 'index.html', {
             'all_banners': all_banners,
